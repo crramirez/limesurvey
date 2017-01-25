99acbda2631b36c26aba9d115492a89aa1a11606
Adam Zammit <adam.zammit@acspri.org.au>
2017-01-25 21:57:50 +1100

    First working of Limesurvey image for container

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,47 @@
-FROM tutum/lamp
+FROM php:5.6-apache
 
-RUN apt-get update ; \
-	apt-get upgrade -q -y ;\
-	apt-get install -q -y curl php5-gd php5-ldap php5-imap unzip wget; apt-get clean ; \
-	php5enmod imap
+ENV DOWNLOAD_URL https://www.limesurvey.org/stable-release?download=1984:limesurvey2620%20170124targz
 
-RUN rm -rf /app 
+# install the PHP extensions we need
+RUN apt-get update && apt-get install -y libfreetype6-dev libmcrypt-dev libpng12-dev libjpeg-dev libldap2-dev zlib1g-dev libkrb5-dev && rm -rf /var/lib/apt/lists/* \
+	&& docker-php-ext-configure gd --with-freetype-dir=/usr/include/  --with-png-dir=/usr --with-jpeg-dir=/usr \
+	&& docker-php-ext-install gd mysqli mysql opcache zip ldap iconv mcrypt
+    && docker-php-ext-configure ldap --with-libdir=lib/x86_64-linux-gnu \ 
+    && docker-php-ext-install ldap \ 
+    && docker-php-ext-configure imap --with-imap-ssl --with-kerberos \ 
+    && docker-php-ext-install imap \
 
-RUN wget https://github.com/LimeSurvey/LimeSurvey/archive/master.zip; \
-    unzip master.zip; \
-    mv LimeSurvey-master /app
+# set recommended PHP.ini settings
+# see https://secure.php.net/manual/en/opcache.installation.php
+RUN { \
+		echo 'opcache.memory_consumption=128'; \
+		echo 'opcache.interned_strings_buffer=8'; \
+		echo 'opcache.max_accelerated_files=4000'; \
+		echo 'opcache.revalidate_freq=2'; \
+		echo 'opcache.fast_shutdown=1'; \
+		echo 'opcache.enable_cli=1'; \
+	} > /usr/local/etc/php/conf.d/opcache-recommended.ini
 
-RUN mkdir -p /uploadstruct; \
-	chown -R www-data:www-data /app
+VOLUME ["/var/www/html"]
 
-RUN cp -r /app/upload/* /uploadstruct ; \
-	chown -R www-data:www-data /uploadstruct
+RUN set -x \
+	&& curl -SL "$DOWNLOAD_URL" -o /tmp/lime.tar.gz \
+    && tar xf /tmp/lime.tar.gz limesurvey --strip-components=1 -C /usr/src/limesurvey \ 
+    && chown -R www-data:www-data /usr/src/limesurvey
 
-RUN chown www-data:www-data /var/lib/php5
+#Set PHP defaults for Limesurvey (allow bigger uploads)
+RUN { \
+		echo 'memory_limit=256M'; \
+		echo 'upload_max_filesize=128M'; \
+		echo 'post_max_size=128M'; \
+		echo 'max_execution_time=120'; \
+        echo 'max_input_vars=10000'; \
+        echo 'date.timezone=UTC'; \
+	} > /usr/local/etc/php/conf.d/uploads.ini
 
-ADD apache_default /etc/apache2/sites-available/000-default.conf
-ADD start.sh /
+COPY docker-entrypoint.sh /usr/local/bin/
+RUN ln -s usr/local/bin/docker-entrypoint.sh /entrypoint.sh # backwards compat
 
-RUN chmod +x /start.sh
-
-VOLUME /app/upload
-
-EXPOSE 80 3306
-CMD ["/start.sh"]
-
+# ENTRYPOINT resets CMD
+ENTRYPOINT ["docker-entrypoint.sh"]
+CMD ["apache2-foreground"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+set -eu
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
+	file_env 'LIMESURVEY_DB_HOST' 'mysql'
+	file_env 'LIMESURVEY_ADMIN_PASSWORD' 'password'
+	# if we're linked to MySQL and thus have credentials already, let's use them
+	file_env 'LIMESURVEY_DB_USER' "${MYSQL_ENV_MYSQL_USER:-root}"
+	if [ "$LIMESURVEY_DB_USER" = 'root' ]; then
+		file_env 'LIMESURVEY_DB_PASSWORD' "${MYSQL_ENV_MYSQL_ROOT_PASSWORD:-}"
+	else
+		file_env 'LIMESURVEY_DB_PASSWORD' "${MYSQL_ENV_MYSQL_PASSWORD:-}"
+	fi
+	file_env 'LIMESURVEY_DB_NAME' "${MYSQL_ENV_MYSQL_DATABASE:-limesurvey}"
+	if [ -z "$LIMESURVEY_DB_PASSWORD" ]; then
+		echo >&2 'error: missing required LIMESURVEY_DB_PASSWORD environment variable'
+		echo >&2 '  Did you forget to -e LIMESURVEY_DB_PASSWORD=... ?'
+		echo >&2
+		echo >&2 '  (Also of interest might be LIMESURVEY_DB_USER and LIMESURVEY_DB_NAME.)'
+		exit 1
+	fi
+
+	if ! [ -e index.php ]; then
+		echo >&2 "Limesurvey not found in $(pwd) - copying now..."
+		if [ "$(ls -A)" ]; then
+			echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
+			( set -x; ls -A; sleep 10 )
+		fi
+		cp -dR /usr/src/limesurvey/* .
+
+		echo >&2 "Complete! Limesurvey has been successfully copied to $(pwd)"
+	else
+        echo >&2 "Limesurvey found in $(pwd) - not copying."
+	fi
+
+    chown www-data:www-data -R tmp 
+    chown www-data:www-data -R upload 
+    chown www-data:www-data -R application/config
+
+	# see http://stackoverflow.com/a/2705678/433558
+	sed_escape_lhs() {
+		echo "$@" | sed -e 's/[]\/$*.^|[]/\\&/g'
+	}
+	sed_escape_rhs() {
+		echo "$@" | sed -e 's/[\/&]/\\&/g'
+	}
+	php_escape() {
+		php -r 'var_export(('$2') $argv[1]);' -- "$1"
+	}
+	set_config() {
+		key="$1"
+		value="$2"
+		sed -i "/$key/s/'[^']*'/'$value'/2" config.inc.php
+	}
+
+	TERM=dumb php -- "$LIMESURVEY_DB_HOST" "$LIMESURVEY_DB_USER" "$LIMESURVEY_DB_PASSWORD" "$LIMESURVEY_DB_NAME" <<'EOPHP'
+<?php
+// database might not exist, so let's try creating it (just to be safe)
+
+$stderr = fopen('php://stderr', 'w');
+
+list($host, $socket) = explode(':', $argv[1], 2);
+$port = 0;
+if (is_numeric($socket)) {
+	$port = (int) $socket;
+	$socket = null;
+}
+
+$maxTries = 10;
+do {
+	$mysql = new mysqli($host, $argv[2], $argv[3], '', $port, $socket);
+	if ($mysql->connect_error) {
+		fwrite($stderr, "\n" . 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
+		--$maxTries;
+		if ($maxTries <= 0) {
+			exit(1);
+		}
+		sleep(3);
+	}
+} while ($mysql->connect_error);
+
+if (!$mysql->query('CREATE DATABASE IF NOT EXISTS `' . $mysql->real_escape_string($argv[4]) . '`')) {
+	fwrite($stderr, "\n" . 'MySQL "CREATE DATABASE" Error: ' . $mysql->error . "\n");
+	$mysql->close();
+	exit(1);
+}
+
+$mysql->close();
+EOPHP
+
+fi
+
+exec "$@"
